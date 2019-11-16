8c695047d62e63553846c31873195190108dac28
David Munro <david.munro@nokia.com>
2019-11-16 10:19:02 -0500

    Updated to font 'Roboto Slab'

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import "milligram";
-import React from "react";
-import ReactDOM from "react-dom";
+import 'milligram'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import App from "./App";
+import App from './App'
+import './styles.css'
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
