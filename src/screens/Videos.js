58681c0dfb1dfaf373a3b1ff0e4ac04e0299df02
David Munro <david.munro@nokia.com>
2019-11-17 16:04:01 -0500

    Update videos screen

--- src/screens/Videos.js
+++ src/screens/Videos.js
@@ -9,16 +9,11 @@ import styled from '@emotion/styled'
 import * as mq from '../media-queries'
 import {db} from '../firebase'
 import YouTube from '../components/YouTube'
-import {CenteredBox} from '../components/components'
 
 const Card = styled.div`
   background: #fff;
   border-radius: 2px;
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-  margin-bottom: 2em;
-  ${mq.small} {
-    margin-bottom: 1em;
-  }
 `
 
 const Videos = () => {
@@ -63,7 +58,17 @@ const Videos = () => {
         <input ref={link} type="text" />
         <button>Add Video</button>
       </form>
-      <div>
+      <div
+        css={css`
+          display: grid;
+          grid-template-columns: repeat(2, 1fr);
+          grid-gap: 1em;
+
+          ${mq.small} {
+            grid-template-columns: repeat(1, 1fr);
+          }
+        `}
+      >
         {error && <strong>Error: {JSON.stringify(error)}</strong>}
         {loading && <span>Loading...</span>}
         {data &&
